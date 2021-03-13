a60ec37d9ad91b6c69bb69fadd89f04875f90ead
Papowe <63293783+Papowe@users.noreply.github.com>
2021-03-13 18:07:33 +0200

    Вынес ввод в другой класс.

--- Gavran.M_Game/Assets/Scripts/Controllers/GameStarter.cs
+++ Gavran.M_Game/Assets/Scripts/Controllers/GameStarter.cs
@@ -9,7 +9,6 @@ namespace Asteroid
             //Enemy.CreateAsteroidEnemy(new Health(100f, 100f));
             EnemyPool enemyPool = new EnemyPool(5);
             var enemy = enemyPool.GetEnemy("Asteroid");
-
         }
     }
 }
---
+++ Gavran.M_Game/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Asteroid
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        public float Horizontal { get; private set; }
+        public float Vertical { get; private set; }
+        public bool Fire { get; private set; }
+
+        private void Update()
+        {
+            Horizontal = Input.GetAxis(InputManagers.HORIZONTAL);
+            Vertical = Input.GetAxis(InputManagers.VERTICAL);
+            Fire = Input.GetButtonDown(InputManagers.FIRE1);
+        }
+    }
+}
